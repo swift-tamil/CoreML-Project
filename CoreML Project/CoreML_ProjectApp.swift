0550f05b900d3ece7b98f46c574ba3825ffc75e6
MohamedGoush <0x1110111@gmail.com>
2023-11-17 00:26:39 +0530

    Initial Commit

---
+++ CoreML Project/CoreML_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreML_ProjectApp.swift
+//  CoreML Project
+//
+//  Created by python on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreML_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
