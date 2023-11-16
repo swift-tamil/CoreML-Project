9d96bc294f9b437fb445c013c0b8a5fcc0543f59
MohamedGoush <0x1110111@gmail.com>
2023-11-17 00:55:19 +0530

    Predicting Image is Perfect But Now We Print It Console

--- CoreML Project/ContentView.swift
+++ CoreML Project/ContentView.swift
@@ -6,14 +6,57 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
+    
+    let images = ["1","2","3","4"]
+    @State private var currentIndex = 0
+    
+    let model = try! MobileNetV2(configuration: MLModelConfiguration())
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Image(images[currentIndex])
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+            HStack{
+                Button("Previous"){
+                    currentIndex -= 1
+                }
+                .buttonStyle(.bordered)
+                .disabled(currentIndex == 0)
+                
+                Button("Next"){
+                    currentIndex += 1
+                }
+                .buttonStyle(.bordered)
+                .disabled(currentIndex == images.count - 1)
+            }
+            Button("Predict"){
+                
+                guard let uiImage = UIImage(named: images[currentIndex]) else {
+                    return
+                }
+                
+                let resizedImage = uiImage.resize(to: CGSize(width: 224, height: 224))
+                
+                guard let buffer = resizedImage.toCVPixelBuffer() else { return }
+                
+                do{
+                    let prediction = try model.prediction(image: buffer)
+                    print(prediction.classLabel)
+                }
+                catch{
+                    print(error.localizedDescription)
+                }
+                
+            }
+            .buttonStyle(.borderedProminent)
+            
+            List(1...10, id: \.self){index in
+                Text("Prediction \(index)")
+            }
         }
         .padding()
     }
